1e5c9c4d5e5ed6fe6da45d5dc0bdd380bb17a5a3
Svetlana <Svetlanka-85@yandex.ru>
2022-05-10 02:57:46 +0500

    feat: add keyboard, add layout change

---
+++ VirtualKeyboard/src/js/Keyboard.js
@@ -0,0 +1,142 @@
+import Key from './key.js';
+import en from './layouts/en.js';
+import ru from './layouts/ru.js';
+
+function set(nameItem, val) {
+    // eslint-disable-next-line no-undef
+  window.localStorage.setItem(nameItem, val);
+}
+  
+function get(nameItem, val) {
+  // eslint-disable-next-line no-undef
+  return (window.localStorage.getItem(nameItem) || val);
+}
+
+// eslint-disable-next-line no-undef
+const body = document.querySelector('body');
+
+export default class Keyboard {
+  constructor(rowsKeybord, lang) {
+    // eslint-disable-next-line no-undef
+    this.lang = lang;
+    this.rowsKeybord = rowsKeybord;
+    const doc = document;
+    const body = doc.querySelector('body');
+    const wrapper = doc.createElement('div');
+    wrapper.classList.add('wrapper');
+    body.prepend(wrapper);
+    this.edit = doc.createElement('textarea');
+    this.edit.classList.add('edit');
+    wrapper.appendChild(this.edit);
+    const keyboardDoc = doc.createElement('div');
+    keyboardDoc.classList.add('Keyboard');
+    wrapper.appendChild(keyboardDoc);
+
+    this.keys = [];
+    this.langLetter = lang==='ru'?ru:en;
+
+    for (let i = 0; i < rowsKeybord.length; i += 1) {
+      const keyboardLine = doc.createElement('div');
+      keyboardLine.classList.add('keyboardLine');
+      keyboardDoc.appendChild(keyboardLine);
+      for (let j = 0; j < rowsKeybord[i].length; j += 1) {
+        const keyObj = this.langLetter.find((value) => value.code === rowsKeybord[i][j]);
+        if (!keyObj) return;
+        const key = new Key(keyObj);
+        //key.keyContent.setAtribute("code", keyObj.code);
+        this.keys.push(key);
+        keyboardLine.appendChild(key.keyContent);
+      }
+    }
+    document.addEventListener('keydown', this.handleEvent);
+    document.addEventListener('keyup', this.handleEvent);
+    document.addEventListener('mousedown', this.handleEvent);
+    document.addEventListener('mouseup', this.handleEvent);
+  }
+    
+  handleEvent = (e) => {
+    if (e.stopPropagation) e.stopPropagation();
+    let code;
+    if (e.type.match(/mouse/) && e.target.closest('.key')){
+        const {dataset: {codeKey}} = e.target.closest('.key');
+        code = codeKey;
+    } else code = e.code;
+    
+    const keyObj = this.keys.find((value) => value.code === code);
+    if (!keyObj) return;
+    this.edit.focus();
+    
+    if (e.type.match(/keydown/)){
+      e.preventDefault();
+      keyObj.keyContent.classList.add('activeKey');
+      if (e.code.match(/Control/)) this.isControl = true;
+      if (e.code.match(/Shift/)) this.isShift = true;
+      if ((this.isControl && e.code.match(/Shift/)) || (this.isShift && e.code.match(/Control/)))
+        this.changeLanguage();
+      this.printKeyboard(keyObj);
+    } else if (e.type.match(/mousedown/)) {
+      keyObj.keyContent.classList.add('activeKey');
+      if (code.match(/Control/)) this.isControl = true;
+      if (code.match(/Shift/)) this.isShift = true;
+      if ((this.isControl && code.match(/Shift/)) || (this.isShift && code.match(/Control/)))
+        this.changeLanguage();
+      this.printKeyboard(keyObj);
+    }
+    else if (e.type.match(/keyup/)){
+      keyObj.keyContent.classList.remove('activeKey');
+      if (e.code.match(/Control/)) this.isControl = false;
+      if (e.code.match(/Shift/)) this.isShift = false;
+    }
+    else if (e.type.match(/mouseup/)){
+        keyObj.keyContent.classList.remove('activeKey');
+        if (keyObj.code.match(/Control/)) this.isControl = false;
+        if (keyObj.code.match(/Shift/)) this.isShift = false;
+      }
+
+    
+  }
+
+  printKeyboard = (keyObj) =>{
+    let cursorPos = this.edit.selectionStart;
+    const leftText = this.edit.value.slice(0, cursorPos);
+    const rightText = this.edit.value.slice(cursorPos);
+    if (keyObj.code === 'Delete') {
+        this.edit.value = leftText + rightText.slice(1);
+    } else if (keyObj.code === 'Backspace'){
+        this.edit.value = leftText.slice(0, -1) + rightText;
+        cursorPos -= 1;
+    } else if (keyObj.code === 'Enter'){
+        this.edit.value = leftText + '\n' + rightText;
+        cursorPos += 1;
+    } else if (keyObj.code === 'Tab'){
+        this.edit.value = leftText + '\t' + rightText;
+        cursorPos += 1;
+    }
+    else{
+        this.edit.value = leftText + keyObj.small + rightText;
+        cursorPos += 1;
+    }
+    this.edit.setSelectionRange(cursorPos, cursorPos);
+  }
+
+  changeLanguage = () => {
+    this.lang = this.lang === "ru"? "en" :"ru";
+    window.localStorage.setItem('LangKeyboard', this.lang);
+    this.langLetter = this.lang === 'ru'?ru:en;
+    for(let i = 0; i < this.keys.length; i += 1){
+        const keyObj = this.langLetter.find((value) => value.code === this.keys[i].code);
+        if (!keyObj) return;
+        this.keys[i].small = keyObj.small;
+        this.keys[i].shift = keyObj.shift;
+        if (this.keys[i].shift && !this.keys[i].code.match(/Arrow/))
+            this.keys[i].keyContent.querySelector(".letter").innerHTML = keyObj.small;
+        if (this.keys[i].shift && this.keys[i].small.match(/[0-9-+=[\];'\\,./`]/)) {
+            this.keys[i].keyContent.querySelector(".keySpec").innerHTML = keyObj.shift;
+        }
+        if (this.keys[i].shift && this.keys[i].small.match(/[ёхъжэбю]/)) {
+            this.keys[i].keyContent.querySelector(".keySpec").innerHTML = '';
+        }
+    }
+  }
+
+}
---
+++ VirtualKeyboard/src/js/key.js
@@ -0,0 +1,59 @@
+export default class Key {
+  constructor({ small, shift, code }) {
+    this.small = small;
+    this.shift = shift;
+    this.code = code;
+
+    // eslint-disable-next-line no-undef
+    const doc = document;
+    const key = doc.createElement('div');
+    key.classList.add('key');
+    key.dataset['codeKey'] = code;
+    const keySpec = doc.createElement('div');
+    keySpec.classList.add('keySpec');
+    if (shift && shift.match(/[^a-zA-Zа-яА-ЯёЁ0-9]/)) {
+      keySpec.textContent = shift;
+    } else {
+      keySpec.textContent = '';
+    }
+    const letter = doc.createElement('div');
+    letter.classList.add('letter');
+    switch (small) {
+      case 'Down': letter.innerHTML = '&darr;'; break;
+      case 'Up': letter.innerHTML = '&uarr;'; break;
+      case 'Left': letter.innerHTML = '&larr;'; break;
+      case 'Right': letter.innerHTML = '&rarr;'; break;
+      case 'Space': break;
+      default: letter.textContent = small;
+    }
+    key.appendChild(keySpec);
+    key.appendChild(letter);
+
+    this.keyContent = key;
+
+    if (small.match(/[a-zа-яё0-9-+`=[\];'\\,./]/)) {
+      key.classList.add('keyLetter');
+    }
+    if (small === 'Delete') {
+      key.classList.add('keyDel');
+    }
+    if (small === 'Backspace' || small === 'Tab' || small === 'CapsLock' || small === 'Enter') {
+      key.classList.add('keySpec2');
+    }
+    if (small === 'Delete') {
+      key.classList.add('keyDel');
+    }
+    if (small === 'Shift') {
+      key.classList.add('keyShift');
+    }
+    if (small === 'Ctrl') {
+      key.classList.add('keyCtrl');
+    }
+    if (small === 'Alt') {
+      key.classList.add('keyAlt');
+    }
+    if (small === 'Space') {
+      key.classList.add('keySpace');
+    }
+  }
+}
